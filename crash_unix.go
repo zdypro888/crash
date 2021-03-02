23c82c247de3a3fa13191326d47bc74fbcf4a424
zdypro <zdypro888@icloud.com>
2021-03-02 19:16:14 +0800

    update crash

--- crash_unix.go
+++ crash_unix.go
@@ -4,15 +4,23 @@ package crash
 
 import (
 	"os"
+	"runtime"
 	"syscall"
 )
 
 //InitPanicFile 初始化 crash 文件
-func InitPanicFile(panicFile string) (*os.File, error) {
-	file, err := os.OpenFile(panicFile, os.O_CREATE|os.O_APPEND, 0666)
+func InitPanicFile(panicFile string) error {
+	file, err := os.OpenFile(panicFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	syscall.Dup2(int(file.Fd()), int(os.Stderr.Fd()))
-	return file, nil
+	if err = syscall.Dup2(int(file.Fd()), int(os.Stderr.Fd())); err != nil {
+		file.Close()
+		return err
+	}
+	// 内存回收前关闭文件描述符
+	runtime.SetFinalizer(file, func(fd *os.File) {
+		fd.Close()
+	})
+	return nil
 }
--- crash_windows.go
+++ crash_windows.go
@@ -12,17 +12,21 @@ const (
 )
 
 //InitPanicFile 初始化 crash 文件
-func InitPanicFile(panicFile string) (*os.File, error) {
+func InitPanicFile(panicFile string) error {
 	file, err := os.OpenFile(panicFile, os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	kernel32 := syscall.NewLazyDLL(kernel32dll)
 	setStdHandle := kernel32.NewProc("SetStdHandle")
 	sh := syscall.STD_ERROR_HANDLE
 	v, _, err := setStdHandle.Call(uintptr(sh), uintptr(file.Fd()))
 	if v == 0 {
-		return nil, err
+		fd.Close()
+		return err
 	}
-	return file, nil
+	runtime.SetFinalizer(file, func(fd *os.File) {
+		fd.Close()
+	})
+	return nil
 }
