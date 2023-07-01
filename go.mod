4742d1f95c0905a83b27d602b2e6e382968f43ca
Pro <zdypro888@icloud.com>
2023-07-01 12:09:46 +0900

    update tag and version

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/zdypro888/crash
 go 1.20
 
 require golang.org/x/sys v0.9.0
+
+require gopkg.in/natefinch/lumberjack.v2 v2.2.1
--- logger.go
+++ logger.go
@@ -2,18 +2,26 @@ package crash
 
 import (
 	"log"
-	"os"
-	"runtime"
+
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func RedirectLog(file string) error {
-	logFile, err := os.OpenFile(file, os.O_CREATE|os.O_WRONLY|os.O_TRUNC|os.O_SYNC, 0644)
-	if err != nil {
-		return err
+	// logFile, err := os.OpenFile(file, os.O_CREATE|os.O_WRONLY|os.O_TRUNC|os.O_SYNC, 0644)
+	// if err != nil {
+	// 	return err
+	// }
+	// runtime.SetFinalizer(logFile, func(fd *os.File) {
+	// 	fd.Close()
+	// })
+	// 创建一个 lumberjack.Logger 实例，用于管理日志文件
+	logFile := &lumberjack.Logger{
+		Filename:   file, // 日志文件名
+		MaxSize:    100,  // 日志文件的最大大小（以MB为单位）
+		MaxBackups: 3,    // 最多保留的旧日志文件数
+		MaxAge:     30,   // 旧日志文件保留的最长天数
+		Compress:   true, // 是否启用压缩旧日志文件
 	}
-	runtime.SetFinalizer(logFile, func(fd *os.File) {
-		fd.Close()
-	})
 	log.SetOutput(logFile)
 	return nil
 }
